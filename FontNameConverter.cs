5d46e39197aea0f2c203df7279ad2e2cec4e49a1
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-03-24 20:37:21 +0800

    Convert font names.

---
+++ FontNameConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace FontNumberViewer
+{
+    internal class FontNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = (FontFamily)value;
+            return v.FamilyNames[XmlLanguage.GetLanguage(culture.Name)];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
